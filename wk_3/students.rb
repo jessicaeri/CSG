89c0430c24b2fcc95ae48285e719fdbb29395b56
jessicaeri <jess.eri0926@gmail.com>
2025-02-10 17:42:06 +1000

    reupload wk 3 project

--- wk_3/students.rb
+++ wk_3/students.rb
@@ -3,32 +3,7 @@
 # Technical Assignment:
 # Create a Student Management System in Ruby. You’ll define a Student class and a Course class, then allow students to enroll in courses and display their information. Below are the steps for completing the core project—and bonus tasks for going above and beyond.
 
-class Course
-  attr_accessor :course_code, :course_name, :enrolled_students
-
-  def initialize(course_code, course_name) #initializes course code and name, but also initializes array for enrolled students within course
-    @course_code = course_code
-    @course_name = course_name
-    @enrolled_students = []
-  end
-
-  def display_info #displays course info
-    puts "Course code: #{course_code.upcase}"
-    puts "Course: #{@course_name} \n "
-  end
-
-  def list_students 
-    if @enrolled_students.empty? #if array is empty displays no students enrolled
-      puts "No students enrolled in this course."
-    else  #if NOT empty displays students within the course 
-      puts "Student enrolled in #{@course_name}:"
-      @enrolled_students.each do |student|
-        puts "  #{student.display_info}"
-      end
-    end
-  end
-
-end
+require_relative 'courses'
 
 class Student 
   attr_accessor :name, :student_id, :courses
